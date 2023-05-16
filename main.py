57de211bd1c9c30d5449725f988aec516500450d
Dallin Reeves <dallinreeves@gmail.com>
2023-05-16 18:04:36 -0500

    set up simple histogram to show count of review stars

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import nltk
 data = pd.read_excel("SampleReviews.xlsx")
 print(data['Score'])
 
+# Create and show histogram showing counts of review stars
 score_plot = data['Score'].value_counts().sort_index().plot(kind='bar',
                                                             title='Count of Reviews', figsize=(10, 5),
                                                             color='blue', edgecolor='black')
