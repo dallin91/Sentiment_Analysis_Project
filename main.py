e493046a6c85745f943f236045072308a278f02c
Dallin Reeves <dallinreeves@gmail.com>
2023-05-16 18:03:06 -0500

    set up simple histogram to show count of review stars

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
 import seaborn as sns
 import nltk
 
 # Read data using pandas
 data = pd.read_excel("SampleReviews.xlsx")
-print(data['Text'])
+print(data['Score'])
+
+score_plot = data['Score'].value_counts().sort_index().plot(kind='bar',
+                                                            title='Count of Reviews', figsize=(10, 5),
+                                                            color='blue', edgecolor='black')
+score_plot.set_xlabel('Review Stars')
+plt.show()
