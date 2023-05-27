d717736f5d4508e7a693801474f897b052e872fb
Dallin Reeves <dallinreeves@gmail.com>
2023-05-27 13:53:37 -0500

    moved figures into a while loop affected by user input to make more interactive/user friendly

--- main.py
+++ main.py
@@ -41,12 +41,12 @@ if not file_path:
 data = pd.read_excel(file_path)
 print(data['Score'])
 
-# Create and show histogram showing counts of review stars
-score_plot = data['Score'].value_counts().sort_index().plot(kind='bar',
-                                                            title='Count of Reviews', figsize=(10, 5),
-                                                            color='gray', edgecolor='black')
-score_plot.set_xlabel('Review Stars')
-plt.show()
+# # Create and show histogram showing counts of review stars
+# score_plot = data['Score'].value_counts().sort_index().plot(kind='bar',
+#                                                             title='Count of Reviews', figsize=(10, 5),
+#                                                             color='gray', edgecolor='black')
+# score_plot.set_xlabel('Review Stars')
+# plt.show()
 
 # NLTK initialization
 example = data['Text'][26]
@@ -124,21 +124,21 @@ tester_plot = sns.barplot(data=roberta, x='Score', y='sentiment')
 tester_plot.set_title('Sentiment Classification by Amazon Star Review')
 plt.show()
 
-# Pie chart showing sentiment
-sentiment_counts = roberta['sentiment'].value_counts()
-plt.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%')
-plt.axis('equal')
-plt.title('Sentiment Breakdown')
-plt.show()
+# # Pie chart showing sentiment
+# sentiment_counts = roberta['sentiment'].value_counts()
+# plt.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%')
+# plt.axis('equal')
+# plt.title('Sentiment Breakdown')
+# plt.show()
 
-# Create word cloud of all reviews
-text = ' '.join(roberta['Text'])
-wordcloud = WordCloud(width=800, height=400).generate(text)
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title('Review World Cloud')
-plt.show()
+# # Create word cloud of all reviews
+# text = ' '.join(roberta['Text'])
+# wordcloud = WordCloud(width=800, height=400).generate(text)
+# plt.figure(figsize=(10, 5))
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis('off')
+# plt.title('Review World Cloud')
+# plt.show()
 
 # Confusion matrix showing Score vs Sentiment
 actual = roberta['sentiment']
@@ -147,27 +147,115 @@ classes = sorted(roberta['sentiment'].unique())
 confusion_mat = confusion_matrix(actual, predicted, labels=classes)
 print(confusion_mat)
 
-plt.imshow(confusion_mat, cmap='Blues')
-plt.grid(False)
-plt.colorbar()
-tick_marks = np.arange(len(classes))
-plt.xticks(tick_marks, classes, rotation=45)
-plt.yticks(tick_marks, classes)
-thresh = confusion_mat.max() / 2
-for i in range(len(classes)):
-    for j in range(len(classes)):
-        # adjust the color of text based on cell color
-        color = 'white' if confusion_mat[i, j] > thresh else 'black'
-        plt.text(j, i, confusion_mat[i, j], ha='center', va='center', color=color)
-
-plt.title('Confusion Matrix')
-plt.xlabel('Predicted')
-plt.ylabel('Actual')
-plt.show()
+# plt.imshow(confusion_mat, cmap='Blues')
+# plt.grid(False)
+# plt.colorbar()
+# tick_marks = np.arange(len(classes))
+# plt.xticks(tick_marks, classes, rotation=45)
+# plt.yticks(tick_marks, classes)
+# thresh = confusion_mat.max() / 2
+# for i in range(len(classes)):
+#     for j in range(len(classes)):
+#         # adjust the color of text based on cell color
+#         color = 'white' if confusion_mat[i, j] > thresh else 'black'
+#         plt.text(j, i, confusion_mat[i, j], ha='center', va='center', color=color)
+#
+# plt.title('Confusion Matrix')
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
 
 # Show accuracy of model
 accuracy = (confusion_mat[0, 0] + confusion_mat[1, 1] + confusion_mat[2, 2]) / confusion_mat.sum()
 print('Model Accuracy: ', accuracy)
+
+vis_input = input("\nWhat further data would you like to see? \n"
+                  "Options:\n"
+                  "Type 'h' to see a histogram of review scores\n"
+                  "Type 'p' to see a pie chart of sentiment classification\n"
+                  "Type 'w' to see a word cloud of the reviews\n"
+                  "Type 'c' to see a confusion matrix\n"
+                  "Type 'n' to save and exit\n")
+
+while vis_input != 'n':
+    if vis_input == 'h':
+        score_plot = data['Score'].value_counts().sort_index().plot(kind='bar',
+                                                                    title='Count of Reviews', figsize=(10, 5),
+                                                                    color='gray', edgecolor='black')
+        score_plot.set_xlabel('Review Stars')
+        plt.show()
+        vis_input = input("\nWhat further data would you like to see? \n"
+                          "Options:\n"
+                          "Type 'h' to see a histogram of review scores\n"
+                          "Type 'p' to see a pie chart of sentiment classification\n"
+                          "Type 'w' to see a word cloud of the reviews\n"
+                          "Type 'c' to see a confusion matrix\n"
+                          "Type 'n' to save and exit\n")
+    if vis_input == 'p':
+        # Pie chart showing sentiment
+        sentiment_counts = roberta['sentiment'].value_counts()
+        plt.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%')
+        plt.axis('equal')
+        plt.title('Sentiment Breakdown')
+        plt.show()
+        vis_input = input("\nWhat further data would you like to see? \n"
+                          "Options:\n"
+                          "Type 'h' to see a histogram of review scores\n"
+                          "Type 'p' to see a pie chart of sentiment classification\n"
+                          "Type 'w' to see a word cloud of the reviews\n"
+                          "Type 'c' to see a confusion matrix\n"
+                          "Type 'n' to save and exit\n")
+    if vis_input == 'w':
+        # Create word cloud of all reviews
+        text = ' '.join(roberta['Text'])
+        wordcloud = WordCloud(width=800, height=400).generate(text)
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        plt.title('Review World Cloud')
+        plt.show()
+        vis_input = input("\nWhat further data would you like to see? \n"
+                          "Options:\n"
+                          "Type 'h' to see a histogram of review scores\n"
+                          "Type 'p' to see a pie chart of sentiment classification\n"
+                          "Type 'w' to see a word cloud of the reviews\n"
+                          "Type 'c' to see a confusion matrix\n"
+                          "Type 'n' to save and exit\n")
+    if vis_input == 'c':
+        plt.imshow(confusion_mat, cmap='Blues', aspect='auto')
+        plt.grid(False)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+        thresh = confusion_mat.max() / 2
+        for i in range(len(classes)):
+            for j in range(len(classes)):
+                # adjust the color of text based on cell color
+                color = 'white' if confusion_mat[i, j] > thresh else 'black'
+                plt.text(j, i, confusion_mat[i, j], ha='center', va='center', color=color)
+
+        plt.title('Confusion Matrix')
+        plt.xlabel('Predicted')
+        plt.ylabel('Actual')
+        plt.show()
+        vis_input = input("\nWhat further data would you like to see? \n"
+                          "Options:\n"
+                          "Type 'h' to see a histogram of review scores\n"
+                          "Type 'p' to see a pie chart of sentiment classification\n"
+                          "Type 'w' to see a word cloud of the reviews\n"
+                          "Type 'c' to see a confusion matrix\n"
+                          "Type 'n' to save and exit\n")
+    if vis_input == 'n':
+        break
+    else:
+        vis_input = input("\nInvalid input. Please select one of the following to continue. \n"
+                          "Options:\n"
+                          "Type 'h' to see a histogram of review scores\n"
+                          "Type 'p' to see a pie chart of sentiment classification\n"
+                          "Type 'w' to see a word cloud of the reviews\n"
+                          "Type 'c' to see a confusion matrix\n"
+                          "Type 'n' to save and exit\n")
 
 # show_prompt = input("Would you like to ask questions about the data? "
 #                                                    "Type 'Yes' or 'No'")
