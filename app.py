8c1eb87c3a850fcc0e4cbfd77fd63a782ac02d34
Dallin Reeves <dallinreeves@gmail.com>
2023-09-22 21:44:07 -0500

    adding routes for app

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Flask!'
+def home():
+    # Add code to render the home page HTML template
+    return render_template('home.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    # Add code to handle file upload and process the Excel file
+    return "Upload route"
+
+@app.route('/analysis')
+def analysis():
+    # Add code to perform sentiment analysis and display results
+    return "Analysis route"
+
+@app.route('/visualizations')
+def visualizations():
+    # Add code to display visualizations
+    return "Visualizations route"
+
+@app.route('/download')
+def download():
+    # Add code to generate and send the Excel file with sentiment analysis results
+    return "Download route"
+
 
 if __name__ == '__main__':
     app.run()
